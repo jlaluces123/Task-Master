bcae21e1ce84735ab7856fd4da649a76949431b0
unknown <jlalucescareer123@gmail.com>
2020-03-18 15:04:34 -0700

    Basic To-Do Functionality: completed

--- todo-intermediate/src/App.js
+++ todo-intermediate/src/App.js
@@ -8,10 +8,33 @@ class App extends React.Component {
         this.state = {
             todos: []
         };
+        this.inputRef = React.createRef();
     }
 
+    saveTodo = async e => {
+        e.preventDefault();
+
+        let todoValue = this.inputRef.current.value;
+
+        this.setState({ todos: [...this.state.todos, todoValue] });
+
+        this.inputRef.current.value = null;
+        return false;
+    };
+
     render() {
-        return <h1>Hello World</h1>;
+        return (
+            <div className='wrapper'>
+                <form onSubmit={this.saveTodo}>
+                    <input type='text' ref={this.inputRef} />
+                    <ul>
+                        {this.state.todos.map((todo, idx) => {
+                            return <li key={idx}>{todo}</li>;
+                        })}
+                    </ul>
+                </form>
+            </div>
+        );
     }
 }
 
