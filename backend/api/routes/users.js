d3d9621911c0e7ef0d41bfe675493375973f567d
unknown <jlalucescareer123@gmail.com>
2020-04-03 10:32:11 -0700

    User Routes: user can now update account information and the password hash middleware will be invoked

--- backend/api/routes/users.js
+++ backend/api/routes/users.js
@@ -3,6 +3,7 @@ const User = require('../../models/user');
 const mongoose = require('mongoose');
 const router = express.Router();
 
+// Get all Users
 router.get('/', (req, res) => {
     User.find()
         .then(users => {
@@ -11,6 +12,7 @@ router.get('/', (req, res) => {
         .catch(err => console.log(err));
 });
 
+// Get a specific User
 router.get('/:userId', (req, res) => {
     const userId = req.params.userId;
     User.find({ _id: userId })
@@ -20,6 +22,12 @@ router.get('/:userId', (req, res) => {
         .catch(err => console.log(err));
 });
 
+// Get a specific User's Todo Lists
+router.get('/:userId/lists', (req, res) => {
+    return res.json({ message: 'Work in progress' });
+});
+
+// Create a new User (login system)
 router.post('/', (req, res) => {
     const user = new User({
         _id: mongoose.Types.ObjectId(),
@@ -35,6 +43,34 @@ router.post('/', (req, res) => {
                 userMade: user
             });
         })
+        .catch(err => console.log(err));
+});
+
+// Edit User details (Update account information)
+router.patch('/:userId', (req, res) => {
+    const userId = req.params.userId;
+
+    User.findById({ _id: userId })
+        .exec()
+        .then(user => {
+            user.username = req.body.username;
+            user.password = req.body.password;
+            return user.save();
+        })
+        .then(response => {
+            console.log(response);
+            res.json(response);
+        })
+        .catch(err => console.log(err));
+});
+
+// Delete your own account
+router.delete('/:userId', (req, res) => {
+    const userId = req.params.userId;
+
+    User.remove({ _id: userId })
+        .exec()
+        .then(response => res.json(response))
         .catch(err => console.log(err));
 });
 
